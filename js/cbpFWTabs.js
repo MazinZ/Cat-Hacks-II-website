fade44a090237843f52a3575a79015407f5cf4ac
Mazin Zakaria <mazinzakaria1@gmail.com>
2015-07-14 19:48:41 -0400

    Small font and animation update

--- js/cbpFWTabs.js
+++ js/cbpFWTabs.js
@@ -63,7 +63,7 @@
 		// change current
 		this.current = idx != undefined ? idx : this.options.start >= 0 && this.options.start < this.items.length ? this.options.start : 0;
 		this.tabs[ this.current ].className = 'tab-current';
-		this.items[ this.current ].className = 'content-current animated slideInLeft';
+		this.items[ this.current ].className = 'content-current animated slideInDown';
 	};
 
 	// add to global namespace
